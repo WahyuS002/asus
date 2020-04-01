278e98f1cce3174c7b117dc891a168cdf72a823a
WahyuS002 <syahputrawahyu61@gmail.com>
2020-04-01 11:19:00 +0700

    API Checkout

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('products', 'API\ProductController@all');
+Route::post('checkout', 'API\CheckoutController@checkout');
